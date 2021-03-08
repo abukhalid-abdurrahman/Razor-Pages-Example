7d4eb981f2618844f2b0310c5d1e31363f8b2ade
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-03-08 19:28:44 +0500

    Fron changes in users list page and simple user(create user) page

--- Razor Pages Examples/Pages/Users/SimpleUser.cshtml.cs
+++ Razor Pages Examples/Pages/Users/SimpleUser.cshtml.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor_Pages_Examples.Models;
 
 namespace Razor_Pages_Examples.Pages.Users
 {
     public class SimpleUserModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public UserEntityModel UserEntityModel { get; set; }
+        public IActionResult OnPost()
         {
-            
+            if (!ModelState.IsValid)
+                return Page();
+            return RedirectToPage("./Users");
         }
     }
 }
